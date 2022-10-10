6188fc362780ac8c3a13e30efa3a86724890fb19
Oleh Tulaidan <oleh.tulaidan@gmail.com>
2022-10-10 15:50:07 +0300

    Import simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryCreating = galleryItems => {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<div class="gallery__item"> 
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}"
+            data-source="${original}" alt="${description}"/>
+        </a>
+    </div>`
+    )
+    .join('');
+};
+
+gallery.insertAdjacentHTML('beforeend', galleryCreating(galleryItems));
+
+gallery.insertAdjacentHTML('beforeend', galleryCreating(galleryItems));
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionType: `attr`,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
