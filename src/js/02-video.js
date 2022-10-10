2f26ed1edd29754d02d307799cc7ddc0bcf13849
Oleh Tulaidan <oleh.tulaidan@gmail.com>
2022-10-10 20:04:12 +0300

    Add videoplayer current time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,50 @@
+const throttle = require('lodash.throttle');
 
+const ACTUAL_PLAYER_TIME_VALUE = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+setPlayerActualTime();
+
+player.on('timeupdate', throttle(timeUpdateHandler, 1000));
+
+function timeUpdateHandler() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      console.log(`Current player time value = ${Math.floor(seconds)} seconds`);
+      setTimeToStorage(seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function setPlayerActualTime() {
+  const actualPlayerTime = getTimeFromStorage();
+  player
+    .setCurrentTime(actualPlayerTime)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
+
+function setTimeToStorage(timeValue) {
+  localStorage.setItem(ACTUAL_PLAYER_TIME_VALUE, timeValue.toString());
+}
+
+function getTimeFromStorage() {
+  return localStorage.getItem(ACTUAL_PLAYER_TIME_VALUE);
+}
